b8537ba070fd1b5d158aa7edbb7e37893a16e70a
maryam <maryamfismaill1@gmail.com>
2024-03-04 13:22:44 -0500

    Initial Commit

---
+++ EndOfYearCooking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EndOfYearCooking
+//
+//  Created by Maryam Ismail on 3/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EndOfYearCooking/EndOfYearCookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EndOfYearCookingApp.swift
+//  EndOfYearCooking
+//
+//  Created by Maryam Ismail on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct EndOfYearCookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
