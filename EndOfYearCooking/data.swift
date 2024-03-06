86f10b539c2a6c2da6e5a7adfca4d250bdf87f21
maryam <maryamfismaill1@gmail.com>
2024-03-06 14:09:53 -0500

    first cooking app upload

---
+++ EndOfYearCooking/data.swift
@@ -0,0 +1,8 @@
+//
+//  data.swift
+//  EndOfYearCooking
+//
+//  Created by Maryam Ismail on 3/4/24.
+//
+
+import Foundation
---
+++ EndOfYearCooking/detailView.swift
@@ -0,0 +1,18 @@
+//
+//  detailView.swift
+//  EndOfYearCooking
+//
+//  Created by Maryam Ismail on 3/4/24.
+//
+
+import SwiftUI
+
+struct detailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    detailView()
+}
