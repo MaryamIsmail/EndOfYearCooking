a6a9f9306988df132c043b3cef70a98d55750df9
maryam <maryamfismaill1@gmail.com>
2024-04-19 14:14:12 -0400

    jhg

---
+++ EndOfYearCooking/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  EndOfYearCooking
+//
+//  Created by Maryam Ismail on 4/18/24.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
